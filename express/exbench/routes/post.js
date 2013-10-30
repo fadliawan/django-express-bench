f7873c717c430a93b522d58a9e7ad4886dd329c8
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2013-10-30 10:35:14 +0700

    Fix indentation again

--- express/exbench/routes/post.js
+++ express/exbench/routes/post.js
@@ -1,27 +1,27 @@
 var Post = require('../models/models').Post;
 
 exports.getJSON = function(req, res) {
-	postLimit = parseInt(req.query.limit, 10);
-	postLimit = !isNaN(postLimit) ? postLimit : 100;
+  postLimit = parseInt(req.query.limit, 10);
+  postLimit = !isNaN(postLimit) ? postLimit : 100;
 
-	Post.find().sort('-pub_date').limit(postLimit)
-	.exec(function(err, posts) {
+  Post.find().sort('-pub_date').limit(postLimit)
+  .exec(function(err, posts) {
 
-		var postsJSON = posts.map(function(post) {
-			return {
-				content: post.content,
-				pub_date: post.pub_date.toString(),
-				comments: post.comments.map(function(comment) {
-					return {
-						author: comment.author,
-						body: comment.body
-					}
-				})
-			};
-		});
+    var postsJSON = posts.map(function(post) {
+      return {
+        content: post.content,
+        pub_date: post.pub_date.toString(),
+        comments: post.comments.map(function(comment) {
+          return {
+          author: comment.author,
+          body: comment.body
+          }
+        })
+      };
+    });
 
-		// Send the data
-		res.json(postsJSON);
+    // Send the data
+    res.json(postsJSON);
 
-	});
+  });
 };
