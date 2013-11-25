abb45d32c42f4be9e4c862bcd87378a3c52a2b27
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2013-11-25 13:57:09 +0700

    Fresh rails install

---
+++ rails/railsbench/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsbench::Application.config.secret_key_base = '4dfffdacfbb2d4682db092bfe500abe800543b7c94b3f1e4adfbcd4c2802bd9a2ebcdbc29af4d7f14cecfbf65fef15740d5df4704abe673b1d4c8d98e34571cf'
